489a0273e65f935a70d0d5040333e6703bc9ea4a
Assem <assem@gmail.com>
2023-11-19 23:00:09 +0600

    step 14

--- src/App.js
+++ src/App.js
@@ -7,20 +7,21 @@ import Dialogs from "./components/Dialogs/Dialogs";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = (props) => {
-    return (
-        <BrowserRouter>
-            <div className='app-wrapper'>
-                <Header />
-                <Navbar />
-                <div className='app-wrapper-content'>
-                    <Routes>
-                        <Route path='/dialogs' element={<Dialogs />} />
-                        <Route path='/profile' element={<Profile />} />
-                    </Routes>
-                </div>
-            </div>
-        </BrowserRouter>
-    );
+  return (
+      <BrowserRouter>
+          <div className='app-wrapper'>
+              <Header />
+              <Navbar />
+              <div className='app-wrapper-content'>
+                  <Routes>
+                      <Route path='/dialogs' element={<Dialogs state={props.state?.dialogsPage} />} />
+                      <Route path='/profile' element={<Profile state={props.state?.profilePage} />} />
+                  </Routes>
+              </div>
+          </div>
+      </BrowserRouter>
+  )
 }
+
 
 export default App;
