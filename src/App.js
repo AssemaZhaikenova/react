7256e100f6030ca72556e235fe50f9e49f3036c9
Assem <assem@gmail.com>
2023-11-19 23:29:47 +0600

    step 18

--- src/App.js
+++ src/App.js
@@ -4,24 +4,21 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from "./components/Dialogs/Dialogs";
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";  // Import Routes
 
 const App = (props) => {
     return (
-            <div className='app-wrapper'>
-                <Header />
-                <Navbar />
-                <div className='app-wrapper-content'>
-                    <Route path='/dialogs'
-                           render={ () => <Dialogs state={props.state.dialogsPage} /> }/>
-                    <Route path='/profile'
-                           render={ () => <Profile
-                               profilePage={props.state.profilePage}
-                               addPost={props.addPost}
-                               updateNewPostText={props.updateNewPostText}
-                           /> }/>
-                </div>
+        <div className='app-wrapper'>
+            <Header />
+            <Navbar />
+            <div className='app-wrapper-content'>
+                <Routes>  {/* Wrap Routes around your Route components */}
+                    <Route path='/dialogs' element={<Dialogs state={props.state.dialogsPage} />} />
+                    <Route path='/profile' element={<Profile profilePage={props.state.profilePage} dispatch={props.dispatch} />} />
+                </Routes>
             </div>
-        )
+        </div>
+    );
 }
+
 export default App;
