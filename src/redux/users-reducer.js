339d641cab106ae4000a0492dc1f67450b2d2d27
Assem <assem@gmail.com>
2023-11-20 00:02:28 +0600

    step 31

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -3,12 +3,11 @@ const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
 const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
 const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
-
 let initialState = {
     users: [ ],
     pageSize: 5,
     totalUsersCount: 0,
-    currentPage: 2
+    currentPage: 1
 };
 
 const usersReducer = (state = initialState, action) => {
@@ -51,5 +50,4 @@ export const unfollowAC = (userId) => ({type: UNFOLLOW, userId })
 export const setUsersAC = (users) => ({type: SET_USERS, users })
 export const setCurrentPageAC = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage })
 export const setUsersTotalCountAC = (totalUsersCount) => ({type: SET_TOTAL_USERS_COUNT, count: totalUsersCount })
-
 export default usersReducer;
