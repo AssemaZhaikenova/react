67c3a54b9350f2ec93eb879c8685703b8f4dc283
Assem <assem@gmail.com>
2023-11-19 18:47:52 +0600

    step 6

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
-
 const MyPosts = (props) => {
   return (
     <div>
@@ -11,10 +10,11 @@ const MyPosts = (props) => {
         <button>Add post</button>
       </div>
       <div className={s.posts}>
-        <Post message='Hi, how are you?' />
-        <Post message="It's my first post" />
+        <Post message='Hi, how are you?' likesCount='0'/>
+        <Post message="It's my first post" likesCount='23' />
       </div>
     </div>
   )
 }
+
 export default MyPosts;
